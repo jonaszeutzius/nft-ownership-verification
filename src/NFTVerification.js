919c8d06c62821a3a75dd7c6b0145cffdba960df
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-15 12:04:29 -0600

    reformatting

--- src/NFTVerification.js
+++ src/NFTVerification.js
@@ -7,29 +7,30 @@ function NFTVerification() {
   const [contract, setContract] = useState('');
   const [data, setData] = useState(null);
 
-const verifyOwnership = async () => {
-  const url = `http://localhost:8080/v1/collections/owner/${address}?chain=eth-main&page_size=25`;
-  const headers = { accept: 'application/json', 'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW' };
-  try {
-    const response = await axios.get(url, { headers });
-    const collectionPromises = response.data.results.map(async (result) => {
-      const collectionUrl = `http://localhost:8080/v1/collections/contract/${result.contract_address}`;
-      const collectionResponse = await axios.get(collectionUrl, { headers });
-      return { ...result, name: collectionResponse.data.name, imageUrl: collectionResponse.data.image_url };
-    });
-    const collectionsWithData = await Promise.all(collectionPromises);
-    setData({ ...response.data, results: collectionsWithData });
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const verifyOwnership = async () => {
+    const url = `http://localhost:8080/v1/collections/owner/${address}?chain=eth-main&page_size=25`;
+    const headers = { accept: 'application/json', 'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW' };
+    try {
+      const response = await axios.get(url, { headers });
+      const filteredResults = response.data.results.filter(result => result.contract_address === contract);
+      const collectionPromises = filteredResults.map(async (result) => {
+        const collectionUrl = `http://localhost:8080/v1/collections/contract/${result.contract_address}`;
+        const collectionResponse = await axios.get(collectionUrl, { headers });
+        return { ...result, name: collectionResponse.data.name, imageUrl: collectionResponse.data.image_url };
+      });
+      const collectionsWithData = await Promise.all(collectionPromises);
+      setData({ ...response.data, results: collectionsWithData });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 return (
     <div>
       <h1 className='title'>NFT Ownership Verification</h1>
-      <p>Input NFT token ID and contract address below to see recent transaction price.</p>
+      <p className='message'>Input a wallet address and contract address below to check if that wallet owns the NFT.</p>
       <div className='inputContainer'>
-        <input type="text" placeholder="Wallet Address" onChange={(e) => setAddress(e.target.value)} />
-        <input type="text" placeholder="Contract Address" onChange={(e) => setContract(e.target.value)} />
+        <input type="text" placeholder="Wallet Address" onChange={(e) => setAddress(e.target.value)} />   
+        <input type="text" placeholder="Contract Address" onChange={(e) => setContract(e.target.value)} />    
         <button onClick={verifyOwnership}>Verify Ownership</button>
       </div>
         {data && (
